30c9bf79dfea8bd6b97a95c2be159f24f359c0a3
byztl <by_ztl@126.com>
2019-03-12 15:18:12 +0800

    initialize

---
+++ live/live/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  live
+//
+//  Created by byztl on 2019/3/12.
+//  Copyright © 2019 byztl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
